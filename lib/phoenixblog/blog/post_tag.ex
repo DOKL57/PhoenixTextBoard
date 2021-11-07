12feb7d193621e7729f061d8431197522e92ef83
D <daniil2705@gmail.com>
2021-11-07 18:03:24 +0300

    Create join schema and migration

---
+++ lib/phoenixblog/blog/post_tag.ex
@@ -0,0 +1,17 @@
+defmodule PhoenixblogWeb.Blog.PostTag do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  schema "posts_tags" do
+    field :post_id, :id, primary_key: true
+    field :tag_id, :id, primary_key: true
+  end
+
+  @doc false
+  def changeset(post_tag, attrs) do
+    post_tag
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
