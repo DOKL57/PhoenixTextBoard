eecc40d4de18716dc8740125c12e87941a462c9e
D <daniil2705@gmail.com>
2021-11-08 08:43:56 +0300

    Add custom tags field

--- lib/phoenixblog_web/live/post_live/form_component.ex
+++ lib/phoenixblog_web/live/post_live/form_component.ex
@@ -3,6 +3,27 @@ defmodule PhoenixblogWeb.PostLive.FormComponent do
 
   alias Phoenixblog.Blog
 
+  alias Phoenixblog.Blog.Tag
+
+  def tags_input(form) do
+    tags_string =
+      form
+      |> input_value(:tags)
+      |> Enum.map(&tag_to_string/1)
+      |> Enum.join(", ")
+
+    text_input(form, :tags, value: tags_string)
+  end
+
+  def tag_to_string(%Ecto.Changeset{} = tag) do
+    Ecto.Changeset.get_field(tag, :name)
+  end
+
+  def tag_to_string(%Tag{} = tag) do
+    tag.name
+  end
+
+
   @impl true
   def update(%{post: post} = assigns, socket) do
     changeset = Blog.change_post(post)
