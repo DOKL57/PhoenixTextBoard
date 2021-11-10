78fc0a456dde75fbd9864e97a67acc2f55e27deb
D <daniil2705@gmail.com>
2021-11-10 07:56:43 +0300

    Fix template (show post and index)

--- priv/repo/migrations/20211107150253_create_posts_tags.exs
+++ priv/repo/migrations/20211107150253_create_posts_tags.exs
@@ -3,7 +3,7 @@ defmodule Phoenixblog.Repo.Migrations.CreatePostsTags do
 
   def change do
     create table(:posts_tags, primary_key: false) do
-      add(:post_id, references(:posts, on_delete: :nothing), null: false, primary_key: true)
+      add(:post_id, references(:posts, on_delete: :delete_all), null: false, primary_key: true)
       add(:tag_id, references(:tags, on_delete: :nothing), null: false, primary_key: true)
     end
 
